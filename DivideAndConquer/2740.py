17ce9d16d2f76288bc5dcee66eda1f2ae9f93989
Luchs41 <kokobo6@gmail.com>
2022-02-26 14:53:22 +0900

    started 2740

---
+++ DivideAndConquer/2740.py
@@ -0,0 +1,10 @@
+# BOJ 2740
+# N*M 크기의 행렬 A와 M*K 크기의 행렬 B의 곱을 구하는 프로그램
+# 첫째 줄에 행렬 A의 크기 N과 M이 주어지고, 다음 N개 줄에 행렬 A의 원소 M개가 순서대로 주어진다. 이어서 행렬 B의 크기 M과 K가 주어지고 이어서 M개의 줄에 행렬 B의 원소 K개가 차례대로 주어진다. 
+import sys
+N, M = map(int, input().split())
+matA = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+M, K = map(int, input().split())
+matB = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+
