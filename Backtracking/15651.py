b98c357979d524d385627d7f1b5160dbcc579a36
Luchs41 <kokobo6@gmail.com>
2022-07-20 21:13:52 +0900

    BOJ 15651 solved

---
+++ Backtracking/15651.py
@@ -0,0 +1,17 @@
+# BOJ 15651
+# 1~N까지의 자연수로 길이가 M인 수열(중복 O)를 모두 구하는 문제. 
+# 백트래킹 알고리즘으로, 리스트에 숫자를 하나씩 추가하며 조건에 부합하는지 확인한다. 
+N, M = map(int, input().split())
+li = []
+
+def DFS() :
+    if len(li) == M :
+        print(' '.join(map(str, li)))
+        return
+    
+    for i in range(1, N + 1) :
+        #if i not in li :
+            li.append(i)
+            DFS()
+            li.pop()
+DFS()
