fd129aa118326da8491545a65f87abd3e5e639c8
Luchs41 <kokobo6@gmail.com>
2023-01-18 11:39:07 +0900

    Sol : BOJ 1918

---
+++ Stack/1918.py
@@ -0,0 +1,41 @@
+# BOJ 1918
+# 입력으로 들어오는 중위 표기식을 후위 표기식으로 바꾸어 출력
+# 피연산자는 바로 출력
+# 연산자는 우선순위에 따라 처리
+# stack이 비어있다면 자신을 push
+# stack top이 자신보다 우선순위가 낮을 때까지 pop(출력) 후 자신을 push
+# 단, '('는 ')'가 아니면 pop 하지 않는다. 
+# ')'가 나오면 '('가 나올 때까지 pop(출력)
+# 문자열이 끝나면 stack이 빌 때까지 pop(출력)
+
+import sys
+
+addop = ['+', '-']
+mulop = ['*', '/']
+
+str_in = sys.stdin.readline().rstrip()
+stack = []
+result = str()
+
+for char in str_in : 
+  if char.isalpha() == True : 
+    result += char
+  else : 
+    if char == '(' : 
+      stack.append(char)
+    elif char in mulop : 
+      while stack and stack[-1] in mulop : 
+        result += stack.pop()
+      stack.append(char)
+    elif char in addop : 
+      while stack and stack[-1] != '(' : 
+        result += stack.pop()
+      stack.append(char)
+    elif char == ')' : 
+      while stack and stack[-1] != '(' : 
+        result += stack.pop()
+      stack.pop()
+
+while stack : 
+  result += stack.pop()
+print(result)
