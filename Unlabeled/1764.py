20b849cae0c6d41fe8cc4014b6c8bd3baa95ff12
Luchs41 <kokobo6@gmail.com>
2023-02-03 14:55:00 +0900

    Fix : BOJ 1764 input

--- Unlabeled/1764.py
+++ Unlabeled/1764.py
@@ -1,16 +1,17 @@
 # BOJ 1764
 # 듣도 못한 사람의 명단과 보도 못한 사람의 명단이 주어질 때
 # 듣도 보도 못한 사람의 명단을 구하는 프로그램
-
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 noListenSee = dict()
 result = []
 for i in range(N) : 
-  name = input()
+  name = input().rstrip()
   noListenSee[name] = True
 
 for i in range(M) : 
-  name = input()
+  name = input().rstrip()
   if noListenSee.get(name, False) == True : 
     result.append(name)
 result.sort()
