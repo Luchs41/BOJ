871c739315ac6f315df88a5fb9c5d7877a3f52ff
Luchs41 <kokobo6@gmail.com>
2023-02-03 10:49:08 +0900

    Sol : BOJ 1764

---
+++ Unlabeled/1764.py
@@ -0,0 +1,19 @@
+# BOJ 1764
+# 듣도 못한 사람의 명단과 보도 못한 사람의 명단이 주어질 때
+# 듣도 보도 못한 사람의 명단을 구하는 프로그램
+
+N, M = map(int, input().split())
+noListenSee = dict()
+result = []
+for i in range(N) : 
+  name = input()
+  noListenSee[name] = True
+
+for i in range(M) : 
+  name = input()
+  if noListenSee.get(name, False) == True : 
+    result.append(name)
+result.sort()
+print(len(result))
+for name in result : 
+  print(name)
